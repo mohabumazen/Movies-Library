1e3c2ff21dea7ac68b298f627bf2cda819fc6bfa
MOHAMMAD ABUMAZEN <moh_abumazen@live.com>
2022-05-01 06:06:08 +0300

    task12

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 const homeData = require("./data.json");
+const axios = require("axios").default;
+const dotenv = require("dotenv");
+const { handle } = require("express/lib/application");
+
+app.use(cors());
+dotenv.config();
+
+// const APIKEY = process.env.APIKEY;
+let apiKey = process.env.APIKEY;
 
 
 app.listen(port,() => {
@@ -13,6 +23,8 @@ app.listen(port,() => {
 
 app.get("/", handleHomePage);
 app.get("/favorite", handleFavoritePage);
+app.get("/trending", handleTrendingPage);
+// app.get("/search", handleSearchPage);
 
 function handleHomePage(req,res) {
 
@@ -39,7 +51,38 @@ function Home(title,poster,overview) {
 
 }
 
+function Tredning(id,title,release,poster,overview){
+    this.id = id;
+    this.title = title;
+    this.release = release;
+    this.poster = poster;
+    this.overview = overview
+}
+
 function handleFavoritePage(req,res) {
 
     res.send("Welcome to Favorite Page");
 }
+
+
+function handleTrendingPage(req,res) {
+    let url = `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}`;
+    axios.get(url)
+        .then(result => {
+            let values = result.data.movies.map(ele => {
+                ele.id,
+                ele.title, 
+                ele.release_date,
+                ele.poster_path,
+                ele.overview
+            })
+        })
+}
+
+
+
+
+
+// function handleSearchPage(req,res) {
+
+// }
