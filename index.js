b8883c5eebdf3fdba2ad9c5da7cf9044e69341a8
MOHAMMAD ABUMAZEN <moh_abumazen@live.com>
2022-05-09 02:43:16 +0300

    finished Task13

--- index.js
+++ index.js
@@ -4,48 +4,63 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios").default;
+const bodyParser = require("body-parser");
 const port = 3000;
 require('dotenv').config()
 const homeData = require("./data.json");
 let apiKey = process.env.API_KEY;
+let urL = process.env.DATABASE_URL;
+const { Client } = require("pg");
+const { query } = require("express");
+const client = new Client(urL);
+
 
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 
 
-app.listen(port,() => {
-    console.log(`server is listening on port ${port}`);
-});
-
-
+// routes
 app.get("/", handleHomePage);
 app.get("/favorite", handleFavoritePage);
 app.get("/trending", tredningHandler);
 app.get("/search", searchHandler);
 app.get("/popular/:page?", popularHandler);
 app.get("/toprated/:page?", topratedHandler);
+app.post("/postMovies", postHandler);
+app.get("/getData", getHandler);
 
 
+// constructor function
+function MovieLibrary(id,title,release,poster,overview) {
+    this.id = id;
+    this.title = title;
+    this.release = release;
+    this.poster = poster;
+    this.overview = overview
+    
+}
 
 function handleHomePage(req,res) {
-
+    
     let arr = [];
     homeData.data.forEach(element => {
-
+        
         let newHome = new Home(
             element.title,
             element.poster_path,
             element.overview
-        );
-
-        arr.push(newHome);
-        console.log(arr);
-        
-    });
-    res.json(arr);
-}
-
+            );
+            
+            arr.push(newHome);
+            console.log(arr);
+            
+        });
+        res.json(arr);
+    }
+    
 function Home(title,poster,overview) {
     this.title = title;
     this.poster = poster;
@@ -54,68 +69,60 @@ function Home(title,poster,overview) {
 }
 
 function handleFavoritePage(req,res) {
-
+    
     res.send("Welcome to Favorite Page3");
 }
 
 function tredningHandler(req,res){
     axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&language=en-US`)
-        .then((trending => {
-            let trendings = trending.data.results.map(Trending => {
-                return new MovieLibrary(
+    .then((trending => {
+        let trendings = trending.data.results.map(Trending => {
+            return new MovieLibrary(
                     Trending.id,
                     Trending.title,
                     Trending.release_date,
                     Trending.poster_path,
                     Trending.overview
-                )
-            })
-            res.json(trendings);
+                    )
+                })
+                res.json(trendings);
+                
+            }))
 
-        }))
-
-        .catch((error => {
-            console.log(error);
+            .catch((error => {
+                console.log(error);
             res.send("error in getting data from API")
         }))
-
+        
 }
 
-function MovieLibrary(id,title,release,poster,overview) {
-    this.id = id;
-    this.title = title;
-    this.release = release;
-    this.poster = poster;
-    this.overview = overview
-
-}
 
 function searchHandler(req,res){
-
+    
     let movieName = req.query.name;
     console.log(movieName);
     let searchResults = [];
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movieName}&page=2`;
     axios.get(url)
-     .then((result) => {
-         result.data.results.forEach((movie) => {
-             movie = new MovieLibrary(
-                 movie.id,
-                 movie.title,
+    .then((result) => {
+        result.data.results.forEach((movie) => {
+            movie = new MovieLibrary(
+                movie.id,
+                movie.title,
                  movie.release_date,
                  movie.poster_path,
                  movie.overview
-
-             );
+                 
+                 );
              searchResults.push(movie);
-         });
-         return res.json(searchResults);
+            });
+            return res.json(searchResults);
      })
      .catch((error) => {
          console.log(error);
          res.send("error in getting data from API")
-     })
-
+        })
+        
 }
 
 function popularHandler(req,res){
@@ -128,35 +135,35 @@ function popularHandler(req,res){
                     popularMovie.release_date,
                     popularMovie.poster_path,
                     popularMovie.overview
-                )
-            })
+                    )
+                })
             res.json(populars);
-
+            
         }))
 
         .catch((error => {
             console.log(error);
             res.send("error in getting data from API")
         }))
-
+        
 }
 
 function topratedHandler(req,res){
     axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=${req.params.page}`)
-        .then((toprated => {
-            let toprateds = toprated.data.results.map(topratedMovie => {
-                return new MovieLibrary(
-                    topratedMovie.id,
-                    topratedMovie.title,
-                    topratedMovie.release_date,
-                    topratedMovie.poster_path,
-                    topratedMovie.overview
+    .then((toprated => {
+        let toprateds = toprated.data.results.map(topratedMovie => {
+            return new MovieLibrary(
+                topratedMovie.id,
+                topratedMovie.title,
+                topratedMovie.release_date,
+                topratedMovie.poster_path,
+                topratedMovie.overview
                 )
             })
             res.json(toprateds);
-
+            
         }))
-
+        
         .catch((error => {
             console.log(error);
             res.send("error in getting data from API")
@@ -164,3 +171,50 @@ function topratedHandler(req,res){
 
 }
 
+function postHandler(req, res){
+    console.log(req.body);
+    
+
+    let {title, length, summary, genres} = req.body;
+    
+
+    let sql = `INSERT INTO moviesdata (title, length, summary, genres)VALUES ($1, $2, $3, $4) RETURNING *;`
+    let values = [title, length, summary, genres];
+    client.query(sql,values).then(result => {
+        console.log(result);
+        return res.status(201).json(result.rows);
+
+    }).catch((err) => {
+        handleError(err, req, res);
+    })
+
+}
+
+function getHandler(req, res){
+    let sql = `SELECT * FROM moviesdata ;`;
+    client.query(sql).then((result) => {
+        console.log(result);
+        res.json(result.rows);
+    }).catch((err) => {
+        handleError(err, req, res);
+    })
+
+}
+
+
+
+// server listener
+client.connect().then(() => {
+
+    app.listen(port,() => {
+            console.log(`server is listening on port ${port}`);
+    });
+}) 
+
+
+
+// error handlers
+
+function handleError(error,req,res){
+    res.status(500).send(error)
+}
