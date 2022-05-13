f273ccb6339b6d093fc94e06352d942ac31b3efa
MOHAMMAD ABUMAZEN <moh_abumazen@live.com>
2022-05-14 01:29:43 +0300

    fixed errors

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS moviesdata;
+
 CREATE TABLE IF NOT EXISTS moviesdata (
     id SERIAL PRIMARY KEY,
     title varchar(255),
     length varchar(255),
     summary varchar(255),
-    genres varchar(255)
+    genres varchar(255),
+    comment varchar(255)
 
-);
+)
